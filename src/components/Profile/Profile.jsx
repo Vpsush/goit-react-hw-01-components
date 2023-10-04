ec589acad642d97d7549a8aef7828453696770c3
Vpsush <vpsush2023@gmail.com>
2023-10-04 20:41:05 +0300

    111

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,31 @@
+import css from './Profile.module.css'
+
+export const Profile = ({text}) = > {
+    return <div className="css.profile">
+    <div className="css.description">
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+        alt="User avatar"
+        className="css.avatar"
+      />
+      <p className="css.name">Petra Marica</p>
+      <p className="css.tag">@pmarica</p>
+      <p className="css.location">Salvador, Brasil</p>
+    </div>
+  
+    <ul className="css.stats">
+      <li>
+        <span className="css.label">Followers</span>
+        <span className="css.quantity">1000</span>
+      </li>
+      <li>
+        <span className="css.label">Views</span>
+        <span className="css.quantity">2000</span>
+      </li>
+      <li>
+        <span className="css.label">Likes</span>
+        <span className="css.quantity">3000</span>
+      </li>
+    </ul>
+  </div>
+}
