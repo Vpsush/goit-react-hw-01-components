b28646591333cbdf49cf22b89f62ade88f1203b3
Vpsush <vpsush2023@gmail.com>
2023-10-09 20:21:27 +0300

    111

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ export const App = () => {
         color: '#010101',
       }}
     >
-      React homework template
+      {/* React homework template */}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from 'App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = document.getElementById('root');
+
+const div = React.createElement('div', {
+  name: 'Profile',
+  class: 'profileClass',
+});
+
+console.log('div :>>', div);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
